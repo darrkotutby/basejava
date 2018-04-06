fbdaeb3aae2951d2cae96274be776b51a3c9c617
Aliaksandr Pilipenka <ext.aliaksandr.pilipenka@tieto.com>
2018-04-06 16:43:26 +0300

    HW1

--- src/main/java/ArrayStorage.java
+++ src/main/java/ArrayStorage.java
@@ -10,49 +10,72 @@ public class ArrayStorage {
 
     private Resume[] storage = new Resume[MAX_SIZE];
 
-    void clear() {
+    public void clear() {
         Arrays.fill(storage, 0, size - 1, null);
         size = 0;
     }
 
-    void save(Resume r) {
-        if (size < MAX_SIZE) {
-            storage[size] = r;
-            size++;
+    public void save(Resume r) {
+        if (size >= MAX_SIZE) {
+            System.out.println("Storage is full");
+            return;
         }
+
+        int i = findResumeElementNumber(r.uuid);
+        if (i != -1) {
+            System.out.println("Resume already exists");
+            return;
+        }
+        storage[size] = r;
+        size++;
     }
 
-    Resume get(String uuid) {
-        for (int i = 0; i < size; i++) {
-            if (storage[i].uuid.equals(uuid)) {
-                return storage[i];
-            }
-        }
+    public Resume get(String uuid) {
+        int i = findResumeElementNumber(uuid);
+        if (i != -1) return storage[i];
+        System.out.println("Resume doesn't exists");
         return null;
     }
 
-    void delete(String uuid) {
-        for (int i = 0; i < size; i++) {
-            if (storage[i].uuid.equals(uuid)) {
-                if (i < size - 1) {
-                    storage[i] = storage[size - 1];
-                }
-                storage[size - 1] = null;
-                size--;
-                return;
+    public void delete(String uuid) {
+        int i = findResumeElementNumber(uuid);
+        if (i != -1) {
+            if (i < size - 1) {
+                storage[i] = storage[size - 1];
             }
-        }
+            storage[size - 1] = null;
+            size--;
+        } else System.out.println("Resume doesn't exists");
+
+
     }
 
     /**
      * @return array, contains only Resumes in storage (without null)
      */
-    Resume[] getAll() {
+    public Resume[] getAll() {
         return Arrays.copyOf(storage, size);
     }
 
-    int size() {
+    public int size() {
         return size;
+    }
+
+    public void update(String oldUUID, String newUUID) {
+        int i = findResumeElementNumber(oldUUID);
+        if (i != -1) {
+            storage[i].uuid = newUUID;
+        } else System.out.println("Resume doesn't exists");
+    }
+
+    private int findResumeElementNumber(String uuid) {
+        for (int i = 0; i < size; i++) {
+            if (storage[i].uuid.equals(uuid)) {
+                return i;
+            }
+
+        }
+        return -1;
     }
 
 }
