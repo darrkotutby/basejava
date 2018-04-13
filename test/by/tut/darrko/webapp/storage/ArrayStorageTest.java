407e5d224b641be4df1910108bf8c2b521d4eff7
Aliaksandr Pilipenka <ext.aliaksandr.pilipenka@tieto.com>
2018-04-13 22:23:45 +0300

    Hw03 refactoring

--- test/by/tut/darrko/webapp/storage/ArrayStorageTest.java
+++ test/by/tut/darrko/webapp/storage/ArrayStorageTest.java
@@ -1,11 +1,7 @@
 package by.tut.darrko.webapp.storage;
 
 public class ArrayStorageTest extends AbstractArrayStorageTest {
-
-
     public ArrayStorageTest() {
         super(new ArrayStorage());
     }
-
-
 }
