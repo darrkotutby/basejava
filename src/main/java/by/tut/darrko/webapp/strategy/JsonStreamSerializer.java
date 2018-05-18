98ee4241ee80028f95f70721542fdfbdb55534e0
Aliaksandr Pilipenka <ext.aliaksandr.pilipenka@tieto.com>
2018-05-18 08:13:24 +0300

    HW10 bugfixing

--- src/main/java/by/tut/darrko/webapp/strategy/JsonStreamSerializer.java
+++ src/main/java/by/tut/darrko/webapp/strategy/JsonStreamSerializer.java
@@ -10,14 +10,14 @@ public class JsonStreamSerializer implements SerializationMethod {
 
     @Override
     public void doWrite(Resume r, OutputStream os) throws IOException {
-        try (Writer writer = new OutputStreamWriter(os, StandardCharsets.UTF_8)) {
+        try (java.io.Writer writer = new OutputStreamWriter(os, StandardCharsets.UTF_8)) {
             JsonParser.write(r, writer);
         }
     }
 
     @Override
     public Resume doRead(InputStream is) throws IOException {
-        try (Reader readerWriter = new InputStreamReader(is, StandardCharsets.UTF_8)) {
+        try (java.io.Reader readerWriter = new InputStreamReader(is, StandardCharsets.UTF_8)) {
             return JsonParser.read(readerWriter, Resume.class);
         }
     }
