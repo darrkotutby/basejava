e7c0b792f867ad7b6076b19df51914afa899eb8e
tpilipenka <tppilipenko@gmail.com>
2018-05-19 22:13:41 +0300

    HW11 add deadlock

--- src/main/java/by/tut/darrko/webapp/MainConcurrency.java
+++ src/main/java/by/tut/darrko/webapp/MainConcurrency.java
@@ -3,10 +3,16 @@ package by.tut.darrko.webapp;
 import java.util.ArrayList;
 import java.util.List;
 
+import static java.lang.Thread.sleep;
+
 public class MainConcurrency {
     public static final int THREADS_NUMBER = 10000;
     private int counter;
     private static final Object LOCK = new Object();
+
+    final Account account1 = new Account();
+    final Account account2 = new Account();
+
 
     public static void main(String[] args) throws InterruptedException {
         System.out.println(Thread.currentThread().getName());
@@ -58,7 +64,41 @@ public class MainConcurrency {
             }
         });
         System.out.println(mainConcurrency.counter);
+
+        Thread thread3 = new Thread(new Runnable() {
+            @Override
+            public void run() {
+                try {
+                    mainConcurrency.account1.addMoney(mainConcurrency.account2, 200);
+                    System.out.println(Thread.currentThread().getName() + ", " + mainConcurrency.account1);
+                    System.out.println(Thread.currentThread().getName() + ", " + mainConcurrency.account2);
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+            }
+        });
+
+        Thread thread4 = new Thread(new Runnable() {
+            @Override
+            public void run() {
+                try {
+                    mainConcurrency.account2.addMoney(mainConcurrency.account1, 300);
+                    System.out.println(Thread.currentThread().getName() + ", " + mainConcurrency.account1);
+                    System.out.println(Thread.currentThread().getName() + ", " + mainConcurrency.account2);
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+            }
+        });
+
+        thread3.start();
+        thread4.start();
+
+        System.out.println(mainConcurrency.account1);
+        System.out.println(mainConcurrency.account2);
+
     }
+
 
     private synchronized void inc() {
 //        synchronized (this) {
@@ -69,4 +109,36 @@ public class MainConcurrency {
 //                ...
 //        }
     }
+
+    class Account {
+        private int amount = 500;
+
+        public int getAmount() {
+            return amount;
+        }
+
+        public synchronized void setAmount(int amount) throws InterruptedException {
+
+            sleep(500);
+            this.amount += amount;
+
+        }
+
+        public synchronized void addMoney(Account anotherAccount, int amount) throws InterruptedException {
+
+            sleep(500);
+            setAmount(amount);
+            anotherAccount.setAmount(-amount);
+
+        }
+
+        @Override
+        public String toString() {
+            return "Account{" +
+                    "amount=" + amount +
+                    '}';
+        }
+    }
+
 }
+
